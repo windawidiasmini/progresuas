4cd1e6acfd8c3860971bc07c87d6f80eb6520c77
windawidiasmini <48646064+windawidiasmini@users.noreply.github.com>
2019-06-14 08:11:58 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {  StyleSheet,  View,  StatusBar } from 'react-native';
+
+import Routes from './progres/Routes';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+           backgroundColor="#1c313a"
+           barStyle="light-content"
+         />
+        <Routes/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+  }
+});
